a44cb516c97c111ce197caf3c93f48e2fa9cc6f3
taotao <zouzhitao_cq@163.com>
2017-08-14 18:26:39 +0800

    添加凸包算法

--- computationGeometry/planeGeometry.cpp
+++ computationGeometry/planeGeometry.cpp
@@ -1,7 +1,25 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define PI acos(-1)
+#define fi first
+#define se second
+#define INF 0x3f3f3f3f
+#define INF64 0x3f3f3f3f3f3f3f3f
+#define random(a,b) ((a)+rand()%((b)-(a)+1))
+#define ms(x,v) memset((x),(v),sizeof(x))
+#define scint(x) scanf("%d",&x );
+#define scf(x) scanf("%lf",&x );
+#define eps 1e-10
+#define dcmp(x) (fabs(x) < eps? 0:((x) <0?-1:1))
+#define torad(x) (PI*x/180)
+#define lc o<<1
+#define rc o<<1|1
+using namespace std;
 #define dcmp(x) (fabs(x) < eps? 0:((x) <0?-1:1))
 struct Point{
     double x,y;
-    Point(double _x=0,double _y=0):x(_x),y(_y){}
+    Point( double _x=0,double _y=0):x(_x),y(_y){}
 };
 typedef Point Vector;
 
@@ -14,7 +32,7 @@ bool operator == (const Point A,const Point  B){return !dcmp(A.x-B.x) && !dcmp(A
 double Dot(Vector A, Vector B){return A.x*B.x+A.y*B.y;}
 double Length(Vector & A){return sqrt(Dot(A,A));}
 double Angle(Vector &A,Vector & B){return acos(Dot(A,B)/Length(A)/Length(B));}
-Vector Rotate(Vector &A,double rad){return Vector(A.x*cos(rad)-sin(rad)*A.y,A.x*sin(rad)+A.y*cos(rad));}
+Vector Rotate(const Vector &A,double rad){return Vector(A.x*cos(rad)-sin(rad)*A.y,A.x*sin(rad)+A.y*cos(rad));}
 Vector Normal(Vector A){//A 的单位法向量
     double L = Length(A);
     return Vector(-A.y/L,A.x/L);
@@ -55,4 +73,73 @@ double polygonArea(Point *p,int n){
     for(int i=1 ; i<n-1 ; ++i)
         area += Cross(p[i]-p[0],p[i+1]-p[0]);
     return area/2;
+}
+//点在多边型内的判定
+int isPointInPolygon(Point P,Point* poly,int n){
+    int wn =0;
+    for(int i=0 ; i<n ; ++i){
+        if(onSegment(P,poly[i],poly[(i+1)%n]))return -1;//在直线上
+        int k = dcmp(Cross(poly[(i+1)%n]-poly[i],P - poly[i]));
+        int d1 = dcmp(poly[i].y - P.y);
+        int d2 = dcmp(poly[(i+1)%n].y - P.y);
+        if(k>0 && d1 <= 0 && d2>0)wn++;//逆时针穿过
+        else if(k<0 && d2<=0 && d1 >0)wn--;
+    }
+    return wn !=0;
+}
+
+int ConvexHull(Point * p,int n,Point* ch){
+    sort(p,p+n);
+    int m=0;
+    for(int i=0 ; i<n ; ++i){
+        while (m>1 && Cross(ch[m-1]-ch[m-2],p[i]-ch[m-2])<=0)m--;//下凸包
+        ch[m++] = p[i];
+    }
+    int k =m;
+    for(int i=n-2 ; i>=0 ; --i){
+        while (m >k && Cross(ch[m-1]-ch[m-2],p[i]-ch[m-2]) <=0)m--;
+        ch[m++] = p[i];
+    }
+    if(n>1)m--;//第一个点
+    return m;
+}
+
+//test
+const int maxn = 2500+10;
+Point p[maxn];
+Point ch[maxn];
+
+int main(int argc, char const *argv[]) {
+    // ios_base::sync_with_stdio(0);
+    // cin.tie(0);
+    // cout.tie(0);
+
+    int T;
+    cin>>T;
+    while (T--) {
+        int n;
+        int cnt =0;
+        cin>>n;
+        double area = 0;
+        for(int i=0 ; i<n ; ++i){
+            double x,y,w,h,j;
+            cin>>x>>y>>w>>h>>j;
+            Point o(x,y);
+            double rad = -torad(j);
+            // std::cout << rad << '\n';
+            // std::cout << "rad" << '\n';
+            // std::cout << j << '\n';
+            p[cnt++] = o+Rotate(Vector(-w/2,-h/2),rad);
+            p[cnt++] = o+Rotate(Vector(-w/2,h/2),rad);
+            p[cnt++] = o + Rotate(Vector(w/2,-h/2),rad);
+            p[cnt++] = o + Rotate(Vector(w/2,h/2),rad);
+            area+= w*h;
+        }
+        // std::cout << area << '\n';
+        int m = ConvexHull(p,cnt , ch);
+        double ans = area / polygonArea(ch,m);
+
+        printf("%.1lf %%\n", ans*100);
+    }
+    return 0;
 }
